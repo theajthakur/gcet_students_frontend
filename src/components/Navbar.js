c2765a6fbab495d82a223cdb4d2ac4f13b030513
Vijay Singh <thakurvijayofficial@gmail.com>
2024-09-16 23:26:23 +0530

    Removed expandation from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,31 +8,18 @@ export default function Navbar() {
         <Link className="navbar-brand" to="/">
           GCET
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/students">
-                Students
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/about">
-                About
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" aria-current="page" to="/students">
+              Students
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" aria-current="page" to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );

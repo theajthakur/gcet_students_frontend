22a07e7f7cb987c2ce6cb37c60961b2353e07a5e
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-10 19:57:01 +0530

    Follow Request Styling

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,21 +1,52 @@
 import React from "react";
-
+import { FaCheck } from "react-icons/fa";
+import "./request.css";
 export default function Request() {
   return (
     <div>
       <div className="follow-requests">
-        <div className="card">
-          <div className="user-info d-flex">
-            <div className="profile-picture">
-              <img
-                src="/image/profile_pic/user.jpg"
-                alt="profile-picture"
-                style={{ width: "50px" }}
-              />
+        <div className="user-card">
+          <div className="user-profile">
+            <img
+              src="/image/profile_pic/user.jpg"
+              style={{ width: "50px" }}
+              alt="Profile Pic"
+            />
+          </div>
+          <div className="user-meta">
+            <div className="user-name">
+              <h3 className="m-0">vijay singh</h3>
             </div>
-            <div className="profile-meta w-100 ps-2 align-items-center d-flex">
-              <h4>Vijay Singh</h4>
+            <div className="user-profile">
+              <p className="m-0">238 followers</p>
             </div>
+          </div>
+          <div className="user-interact ms-auto">
+            <button className="btn btn-success">
+              <FaCheck className="icon" />
+            </button>
+          </div>
+        </div>
+        <div className="user-card">
+          <div className="user-profile">
+            <img
+              src="/image/profile_pic/user.jpg"
+              style={{ width: "50px" }}
+              alt="Profile Pic"
+            />
+          </div>
+          <div className="user-meta">
+            <div className="user-name">
+              <h3 className="m-0">vijay singh</h3>
+            </div>
+            <div className="user-profile">
+              <p className="m-0">238 followers</p>
+            </div>
+          </div>
+          <div className="user-interact ms-auto">
+            <button className="btn btn-success">
+              <FaCheck className="icon" />
+            </button>
           </div>
         </div>
       </div>
